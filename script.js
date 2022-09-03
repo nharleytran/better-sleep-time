509a553830adf80f880c1017c9008b8804e37982
nharleytran <98291123+nharleytran@users.noreply.github.com>
2022-09-02 21:37:01 -0400

    alternate color of hours

---
+++ script.js
@@ -0,0 +1,32 @@
+const colors = [
+  "white",
+  "red",
+  "white",
+  "red",
+  "white",
+  "red",
+  "white",
+  "red",
+]
+
+function handleOnClick() {
+    let output = document.querySelector(".output");
+    output.style.display = "block";
+  
+    let hours = document.getElementById("hours");
+    hours.innerHTML = "";
+  
+    let now = new Date();
+    // allow 14 minutes to fall sleep!
+    now.setMinutes(now.getMinutes() + 14);
+  
+    // calculate sleep cycles!
+    for (let i = 1; i <= 6; i++) {
+      now.setMinutes(now.getMinutes() + 90);
+      let elm = document.createElement("div");
+      elm.innerText = 
+        now.toLocaleTimeString("en-US", { timeStyle: "short" }) + "<br />";
+      elm.style.color = colors[i];
+      hours.appendChild(elm);
+    } 
+  }
